93fe13633ff252aabbeb585b77b3103bbe6ffb7b
Neto Gerbi <netogerbi2@hotmail.com>
2018-07-28 18:38:17 -0300

    Banco de dados H2 e Mapeamento do objeto relacional Categoria (Criação automatica do BD)

--- src/main/java/com/netogerbi/cursomc/domain/Categoria.java
+++ src/main/java/com/netogerbi/cursomc/domain/Categoria.java
@@ -2,9 +2,19 @@ package com.netogerbi.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//mapeamento Objeto relacional
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	//mapeamento Objeto relacional
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
 	private Integer ID;
 	private String name;
 	
