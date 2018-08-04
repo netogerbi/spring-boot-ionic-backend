08c8d245823010d2874104a6d2f836e803739745
Neto Gerbi <netogerbi2@hotmail.com>
2018-08-04 17:12:43 -0300

    Inclusão de Classes para tratamento de exceptions em endpoint: /categorias/{id}

--- src/main/java/com/netogerbi/cursomc/services/CategoriaService.java
+++ src/main/java/com/netogerbi/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.netogerbi.cursomc.domain.Categoria;
 import com.netogerbi.cursomc.repositories.CategoriaRepository;
+import com.netogerbi.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,6 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: "+id+", Tipo:" + Categoria.class.getName()));
 	}
 }
