17222c9bf294ebd68d302a7f35b6df988b11a419
Neto Gerbi <netogerbi2@hotmail.com>
2018-09-13 20:50:10 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/netogerbi/cursomc/CursomcApplication.java
+++ src/main/java/com/netogerbi/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.netogerbi.cursomc.domain.Cidade;
 import com.netogerbi.cursomc.domain.Cliente;
 import com.netogerbi.cursomc.domain.Endereco;
 import com.netogerbi.cursomc.domain.Estado;
+import com.netogerbi.cursomc.domain.ItemPedido;
 import com.netogerbi.cursomc.domain.Pagamento;
 import com.netogerbi.cursomc.domain.PagamentoComBoleto;
 import com.netogerbi.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.netogerbi.cursomc.repositories.CidadeRepository;
 import com.netogerbi.cursomc.repositories.ClienteRepository;
 import com.netogerbi.cursomc.repositories.EnderecoRepository;
 import com.netogerbi.cursomc.repositories.EstadoRepository;
+import com.netogerbi.cursomc.repositories.ItemPedidoRepository;
 import com.netogerbi.cursomc.repositories.PagamentoRepository;
 import com.netogerbi.cursomc.repositories.PedidoRepository;
 import com.netogerbi.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRespository;
 	
 	
 	
@@ -113,6 +117,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 200.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRespository.saveAll(Arrays.asList(ip1,ip2,ip3));
 				
 	}
 	
--- src/main/java/com/netogerbi/cursomc/domain/Produto.java
+++ src/main/java/com/netogerbi/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.netogerbi.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -31,15 +34,34 @@ public class Produto implements Serializable {
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
 			)
 	private List<Categoria> categorias = new ArrayList<>();
-
-	public Produto() {
-	}
+	
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Produto(Integer id, String nome, double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List <Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
+	public Produto() {
 	}
 
 	public Integer getId() {
@@ -98,6 +120,7 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
+
 	
 	
 }
