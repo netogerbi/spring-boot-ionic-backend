64405d9dd42fd811a96c965867b6c3e2319fa698
Neto Gerbi <netogerbi2@hotmail.com>
2018-09-20 19:38:23 -0300

    Insrindo nova Categoria com POST

--- src/main/java/com/netogerbi/cursomc/domain/Categoria.java
+++ src/main/java/com/netogerbi/cursomc/domain/Categoria.java
@@ -17,8 +17,9 @@ public class Categoria implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY) 
-	private Integer ID;
-	private String name;
+	private Integer id;
+	
+	private String nome;
 	
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
@@ -26,33 +27,33 @@ public class Categoria implements Serializable {
 	public Categoria() {
 	}
 
-	public Categoria(Integer iD, String name) {
+	public Categoria(Integer id, String nome) {
 		super();
-		ID = iD;
-		this.name = name;
+		this.id = id;
+		this.nome = nome;
 	}
 
 	public Integer getID() {
-		return ID;
+		return id;
 	}
 
-	public void setID(Integer iD) {
-		ID = iD;
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getName() {
-		return name;
+		return nome;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String nome) {
+		this.nome = nome;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((ID == null) ? 0 : ID.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 	
@@ -73,10 +74,10 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		if (ID == null) {
-			if (other.ID != null)
+		if (id == null) {
+			if (other.id != null)
 				return false;
-		} else if (!ID.equals(other.ID))
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
 	}
