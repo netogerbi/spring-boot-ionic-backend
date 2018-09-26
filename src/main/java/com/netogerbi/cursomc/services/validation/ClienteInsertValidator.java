2d07d00b5b216b5293b41de7f9db41c3673e6a6a
Neto Gerbi <netogerbi2@hotmail.com>
2018-09-26 18:56:26 -0300

    Inserção de regra de validação no e-mail

--- src/main/java/com/netogerbi/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/netogerbi/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.netogerbi.cursomc.domain.Cliente;
 import com.netogerbi.cursomc.domain.enums.TipoCliente;
 import com.netogerbi.cursomc.dto.ClienteNewDTO;
+import com.netogerbi.cursomc.repositories.ClienteRepository;
 import com.netogerbi.cursomc.resources.exceptions.FieldMessage;
 import com.netogerbi.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -24,9 +32,16 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
+		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já Existente!"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
