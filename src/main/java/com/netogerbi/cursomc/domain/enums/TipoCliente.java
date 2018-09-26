a79187566b8af9c916a4a1ddd4777ba0e2b38fb5
Neto Gerbi <netogerbi2@hotmail.com>
2018-09-25 22:42:17 -0300

    Validação Customizada de CPF e CNPJ na Inserção de Clientes (ERRO 500)

--- src/main/java/com/netogerbi/cursomc/domain/enums/TipoCliente.java
+++ src/main/java/com/netogerbi/cursomc/domain/enums/TipoCliente.java
@@ -3,7 +3,7 @@ package com.netogerbi.cursomc.domain.enums;
 public enum TipoCliente {
 	
 	PESSOAFISICA(1, "Pessoa Física"),
-	PESSOAJURIDICA(2, "Pessoa Física");
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
 	
 	private Integer cod;
 	private String descricao;
--- src/main/java/com/netogerbi/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/netogerbi/cursomc/dto/ClienteNewDTO.java
@@ -2,24 +2,52 @@ package com.netogerbi.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.netogerbi.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message="Preenchimento Obrigatório!")
+	@Length(min=5,max=120)
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
+	@Email(message="Email Inválido!")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String cpfOuCnpj;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String numero;
+	
 	private String complemento;
+	
+	
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private Integer cidadeId;
 	
 	
