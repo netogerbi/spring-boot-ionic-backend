7bbdd5c9386e4132a8632f7b55fe3121145c0033
Neto Gerbi <netogerbi2@hotmail.com>
2018-09-29 15:02:50 -0300

    Inserindo pedido

--- src/main/java/com/netogerbi/cursomc/domain/ItemPedido.java
+++ src/main/java/com/netogerbi/cursomc/domain/ItemPedido.java
@@ -35,6 +35,14 @@ public class ItemPedido implements Serializable {
 		return (preco-desconto)*quantidade;
 	}
 	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
--- src/main/java/com/netogerbi/cursomc/services/PedidoService.java
+++ src/main/java/com/netogerbi/cursomc/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.netogerbi.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netogerbi.cursomc.domain.ItemPedido;
+import com.netogerbi.cursomc.domain.PagamentoComBoleto;
 import com.netogerbi.cursomc.domain.Pedido;
+import com.netogerbi.cursomc.domain.enums.EstadoPagamento;
+import com.netogerbi.cursomc.repositories.ItemPedidoRepository;
+import com.netogerbi.cursomc.repositories.PagamentoRepository;
 import com.netogerbi.cursomc.repositories.PedidoRepository;
 import com.netogerbi.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -13,10 +19,42 @@ import com.netogerbi.cursomc.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 
 	@Autowired
-	private PedidoRepository repo;
+	private PedidoRepository repository;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public Pedido find(Integer id) {
-		Optional<Pedido> obj = repo.findById(id);
+		Optional<Pedido> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+id+" Tipo: "+Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pgto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pgto, obj.getInstante());
+		}
+		repository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
