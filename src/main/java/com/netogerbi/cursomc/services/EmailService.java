2b6382d64296ed81cf5176786cefb00f31fc3a67
Neto Gerbi <netogerbi2@hotmail.com>
2018-10-03 21:45:55 -0300

    MockEmailService com Logger. Padrões Strategy e Template Mehtos

---
+++ src/main/java/com/netogerbi/cursomc/services/EmailService.java
@@ -0,0 +1,10 @@
+package com.netogerbi.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.netogerbi.cursomc.domain.Pedido;
+
+public interface EmailService {
+	void sendOrderConfirmationEmail(Pedido obj);
+	void sendEmail(SimpleMailMessage msg);
+}
