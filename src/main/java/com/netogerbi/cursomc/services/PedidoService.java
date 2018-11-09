e61e48c6cb3dc0d5ff84a2202cdaf59653a23ee0
Neto Gerbi <netogerbi2@hotmail.com>
2018-11-09 21:45:43 -0200

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/netogerbi/cursomc/services/PedidoService.java
+++ src/main/java/com/netogerbi/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.netogerbi.cursomc.domain.Cliente;
 import com.netogerbi.cursomc.domain.ItemPedido;
 import com.netogerbi.cursomc.domain.PagamentoComBoleto;
 import com.netogerbi.cursomc.domain.Pedido;
@@ -13,14 +17,15 @@ import com.netogerbi.cursomc.domain.enums.EstadoPagamento;
 import com.netogerbi.cursomc.repositories.ItemPedidoRepository;
 import com.netogerbi.cursomc.repositories.PagamentoRepository;
 import com.netogerbi.cursomc.repositories.PedidoRepository;
+import com.netogerbi.cursomc.security.UserSS;
+import com.netogerbi.cursomc.services.exceptions.AuthorizationException;
 import com.netogerbi.cursomc.services.exceptions.ObjectNotFoundException;
-import com.netogerbi.cursomc.services.EmailService;
 
 @Service
 public class PedidoService {
 
 	@Autowired
-	private PedidoRepository repository;
+	private PedidoRepository repo;
 	
 	@Autowired
 	private BoletoService boletoService;
@@ -41,7 +46,7 @@ public class PedidoService {
 	private EmailService emailService;
 	
 	public Pedido find(Integer id) {
-		Optional<Pedido> obj = repository.findById(id);
+		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+id+" Tipo: "+Pedido.class.getName()));
 	}
 	
@@ -55,7 +60,7 @@ public class PedidoService {
 			PagamentoComBoleto pgto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pgto, obj.getInstante());
 		}
-		repository.save(obj);
+		repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
 		for(ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
@@ -66,5 +71,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
