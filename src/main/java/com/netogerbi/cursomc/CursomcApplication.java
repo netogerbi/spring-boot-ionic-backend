293a696f7290b0f2102b98eeca1dd050483d51d9
Neto Gerbi <netogerbi2@hotmail.com>
2018-11-21 20:17:02 -0200

    Enviando Imagem via Endpoint

--- src/main/java/com/netogerbi/cursomc/CursomcApplication.java
+++ src/main/java/com/netogerbi/cursomc/CursomcApplication.java
@@ -1,17 +1,12 @@
 package com.netogerbi.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.netogerbi.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {	
 	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,6 +14,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\netog\\Desktop\\Curso Spring Boot Ionic - Udemy\\3D.png");
+		
 	}
+	
 }
